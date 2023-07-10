ae5e5500900f953b17897a816d23b8781c709b58
Dharsan15 <dharsanravi6677@gmail.com>
2023-07-10 22:50:11 +0530

    file modified

--- streams-practice.js
+++ streams-practice.js
@@ -1,24 +1,21 @@
-const fs = require('fs');
-
-
+const fs = require("fs");
 
 //creating a read stream
-const readstream = fs.createReadStream('./read.txt')
-
+const readstream = fs.createReadStream("./read.txt");
 
 // creating a write stream
-const writestream = fs.createWriteStream('./read1.txt');
+const writestream = fs.createWriteStream("./read2.txt");
+
+readstream.on("data", (chunk) => {
+  // getting a chunk of data and printing it
+  console.log(chunk.toString());
+
+  // writing the chunk in another file
+  writestream.write("\n new chunk added \n");
+  writestream.write(chunk);
+});
 
 
-readstream.on('data' , (chunk) => {
-   
-    // getting a chunk of data and printing it 
-       console.log(chunk.toString());
+// doing the above task using piping 
 
-       // writing the chunk in another file 
-        writestream.write('\n new chunk added \n')
-        writestream.write(chunk)
-})
-
-
-
+readstream.pipe(writestream);
