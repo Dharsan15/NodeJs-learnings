c012c0ca0f41ad1acfeca6fb87177c2479dd7e6b
Dharsan15 <dharsanravi6677@gmail.com>
2023-07-10 22:44:22 +0530

    new commit

---
+++ streams-practice.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+
+
+//creating a read stream
+const readstream = fs.createReadStream('./read.txt')
+
+
+// creating a write stream
+const writestream = fs.createWriteStream('./read1.txt');
+
+
+readstream.on('data' , (chunk) => {
+   
+    // getting a chunk of data and printing it 
+       console.log(chunk.toString());
+
+       // writing the chunk in another file 
+        writestream.write('\n new chunk added \n')
+        writestream.write(chunk)
+})
+
+
+
