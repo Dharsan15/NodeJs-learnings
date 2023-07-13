9cf521d701f9676885c350150402086484311aca
Dharsan15 <dharsanravi6677@gmail.com>
2023-07-13 15:42:38 +0530

    new commit

---
+++ routing.js
@@ -0,0 +1,42 @@
+// lets do a very basic routing
+
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log("request made", req.url);
+
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./src/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/contact":
+      path += "contact.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
+
+server.listen(5000, "localhost", () => {
+  console.log("listening to the port 3000");
+});
--- server.js
+++ server.js
@@ -1,58 +1,49 @@
 // creating our server using node
 
-// first we should import the http module 
+// first we should import the http module
 
-const http = require('http')
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req , res) => {
-    console.log('request made');
+const server = http.createServer((req, res) => {
+  console.log("request made");
 
-    // listening  to the port and local host
-    // this will atlest 1 parameter inside it
-    server.listen(3000 , 'localhost' , () => {
-        console.log('listening to the local host');
-    })
+  // listening  to the port and local host
+  // this will atlest 1 parameter inside it
+  
 
-    // responding to the requests (this is just a text response)
+  // responding to the requests (this is just a text response)
 
-    // First we should set the header to tell what content we are sending as response
-    // in this case we are responding with plain text as response
-        
-    res.setHeader('Content-Type' , 'text/plain' );
-     res.write("you have made a succesful request");
-    res.end();
-    
-// -----------------------------------------------------------------------------------------------------
-// responding with html code from another file
-    
-// in this case we are sending a html page as response
-    res.setHeader('Content-Type' , 'text/html' );
+  // First we should set the header to tell what content we are sending as response
+  // in this case we are responding with plain text as response
 
+  res.setHeader("Content-Type", "text/plain");
+  res.write("you have made a succesful request");
+  res.end();
 
-    // first reading the file 
-    fs.readFile('./src/index.html' , (err , data) => {
-        if(err)
-        {
-            console.log(err);
-            res.end();
-        }
-        
-        else
-        {
-        // then writing the file in the browser
-            res.write(data);
+  // -----------------------------------------------------------------------------------------------------
+  // responding with html code from another file
 
-            // ending the request
+  // in this case we are sending a html page as response
+  res.setHeader("Content-Type", "text/html");
 
-             res.end();
-        }
-    });
-    
-   
+  // first reading the file
+  fs.readFile("./src/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      // then writing the file in the browser
+      res.write(data);
 
-})
+      // ending the request
+
+      res.end();
+    }
+  });
+});
 
 
-
-
+server.listen(3000, "localhost", () => {
+  console.log("listening to the local host");
+});
